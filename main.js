734b37f4665fbc11fc42cef5337c0e2f6f885bda
BlakeR06 <113364139+BlakeR06@users.noreply.github.com>
2023-09-05 13:18:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function returnToMenu(){
+    window.location.href = 'index.html';
+}
+
+function newForFall(){
+    window.location.href = 'newForFall.html';
+}
+
+function familyMeals(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src2/familyMeals.html';
+}
+
+function breakfastAllDay(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src2/breakfastAllDay.html';
+}
+
+function chickenNTurkey(){
+    window.location.href = 'chickenNTurkey.html';
+}
+
+function beefNPork(){
+    window.location.href = 'beefNPork.html';
+}
+
+function seafood(){
+    window.location.href = 'seafood.html';
+}
+
+function burgersSaladsNMore(){
+    window.location.href = 'burgersSaladsNMore.html';
+}
+
+function dailySpecials(){
+    window.location.href = 'dailySpecials.html';
+}
+
+function individualSides(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src3/individualSides.html';
+}
+
+function barrelBites(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src3/barrelBites.html';
+}
+
+function drinks(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src3/drinks.html';
+}
+
+function desserts(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src2/desserts.html';
+}
+
+function kids(){
+    window.location.href = 'https://blaker06.github.io/Cracker-Barrel-Menu-Src2/kids.html';
+}
